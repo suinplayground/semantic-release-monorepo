3f5595d6003bd9175d900ac48aeb24784bb458c0
suin <suinyeze@gmail.com>
2022-02-04 08:50:45 +0900

    fix(a): 🐛 fix something
    ✅ Closes: #1

--- commitlint.config.js
+++ commitlint.config.js
@@ -11,12 +11,14 @@ const subjectMaxLength = require("./changelog.config").maxMessageLength ?? 64;
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   /**
-   * @see {@link https://commitlint.js.org/#/reference-configuration} for rule details.
+   * @see {@link https://commitlint.js.org/#/reference-rules} for rule details.
    */
   rules: {
     "type-enum": [2, "always", typeEnum],
     "scope-enum": [2, "always", scopeEnum],
     "subject-min-length": [2, "always", subjectMinLength],
     "subject-max-length": [2, "always", subjectMaxLength],
+    "body-max-length": [0],
+    "body-max-line-length": [0],
   },
 };
